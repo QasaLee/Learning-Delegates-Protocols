efdaddd98f43dfc16d8c9d7a65fe2bd1a7e6c78c
NateLee <intoqasa@gmail.com>
2018-09-10 09:09:28 +0800

    Added Base & Selection VCs.

---
+++ Delegates-Protocols/SelectionScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SelectionScreen.swift
+//  Delegates-Protocols
+//
+//  Created by M78 on 9/10/18.
+//  Copyright © 2018 Sean Allen. All rights reserved.
+//
+
+import UIKit
+
+class SelectionScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
